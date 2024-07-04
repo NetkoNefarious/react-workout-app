2e61ecb62c07c8ca6bc2e10d762755cde6817beb
Jakov Magazin <89070980+jmagazin-as24@users.noreply.github.com>
2024-07-04 10:31:49 +0200

    Initial commit

---
+++ solid-workout-app/src/components/Workout.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import ExerciseCard from './ExerciseCard'
+import SectionWrapper from './SectionWrapper'
+
+export default function Workout({ workout }) {
+    return (
+        <SectionWrapper id='workout' header="Welcome to" title={["The", "DANGER", "zone"]}>
+            <div className='flex flex-col gap-4'>
+                {workout.map((exercise, i) => (
+                    <ExerciseCard index={i} exercise={exercise} key={i} />
+                ))}
+            </div>
+        </SectionWrapper>
+    )
+}
