33fc75ea4b194ff7a5884dee423c919c2ddeec41
Jakov Magazin <89070980+jmagazin-as24@users.noreply.github.com>
2024-07-04 13:54:50 +0200

    Remove SolidJS

--- solid-workout-app/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function Button({ children, func }) {
-    return (
-        <button onClick={func} className="px-8 mx-auto py-4 rounded-md border-[2px] border-blue-400 border-solid bg-slate-950 blueShadow duration-200">
-            <p>{children}</p>
-        </button>
-    )
-}
